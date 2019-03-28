3e94851f97d268634be95472555bf6118a668917
monicagomezavila <monicagomezavila@github.com>
2019-03-28 18:50:32 +0100

    Looping del vídoe principal, el mute no funciona

--- video.js
+++ video.js
@@ -6,16 +6,14 @@ function main()
   video1 = document.getElementById("video1")
   video1.width = 200;
   video1.height= 100;
-  // VÍDEO 1 REPRODUCIENDO
   play1 = document.getElementById('play1');
-  video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
 
 
   // VÍDEO 2 REPRODUCIENDO
   video2 = document.getElementById("video2")
   video2.width = 200;
   video2.height= 100;
-
   play2 = document.getElementById('play2');
   video2.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
 
@@ -23,37 +21,67 @@ function main()
   video3 = document.getElementById("video3")
   video3.width = 200;
   video3.height= 100;
-
   play3 = document.getElementById('play3');
   video3.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
 
   // VÍDEO PRINCIPAL
   video4 = document.getElementById("video4")
-  video4.width = 400;
-  video4.height= 200;
+  video4.width = 600;
+  video4.height= 400;
   video4.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
 
+  //CAMBIANDO EL VÍDEO A REPRODUCIR
   play1.onclick = () => {
     console.log("Ver 1!")
-    //-- Indicar la fuente del vídeo
     video4.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
   }
 
   play2.onclick = () => {
     console.log("Ver 2!")
-    //-- Indicar la fuente del vídeo
     video4.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
   }
 
   play3.onclick = () => {
     console.log("Ver 3!")
-    //-- Indicar la fuente del vídeo
     video4.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
 
   }
 
-  video4.addEventListener("loadeddata",function(ev){
-   document.getElementById("tduracion").innerHTML = video4.duration;
+  function hora(segundos){
+     var d=new Date(segundos*1000);
+     var hora = (d.getHours()==0)?23:d.getHours()-1;
+     var hora = (hora<9)?"0"+hora:hora;
+     var minuto = (d.getMinutes()<9)?"0"+d.getMinutes():d.getMinutes();
+     var segundo = (d.getSeconds()<9)?"0"+d.getSeconds():d.getSeconds();
+     return hora+":"+minuto+":"+segundo;
+    }
+
+
+//DURACIÓN DEL VÍDEO
+ video4.addEventListener("timeupdate",function(ev){
+   document.getElementById("tduracion").innerHTML = hora(video4.currentTime);
   },true);
 
+
+//VÍDEO INICIAL EN LOOPING O NO
+  var loop = document.getElementById('loop');
+
+  loop.onclick = () => {
+    console.log("looping!");
+    video4.loop = true;
+  }
+
+  var noloop = document.getElementById("noloop");
+
+  noloop.onclick = () => {
+    console.log("no looping!")
+    video4.loop = false;
+  }
+
+
+//CUANDO PASAS EL RATÓN EL VÍDEO TIENE SONIDO
+  video1.onmouseover = () => {
+    console.log("Mouse over!!")
+    //video1.muted = false;
+  }
 }
