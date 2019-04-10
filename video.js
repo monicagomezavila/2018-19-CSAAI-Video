b90fcc202a100496e374f6facfc8599e95f53752
monicagomezavila <monicagomezavila@github.com>
2019-04-10 15:19:27 +0200

    muted no funciona y puesto recuadro

--- video.js
+++ video.js
@@ -17,9 +17,19 @@ function main()
     this.principal = function(video4){
       video4.src  = this.video.src;
       video4.currentTime = this.video.currentTime;
+    }
+
+    this.styleput = function(){
       this.video.style.border = '3px solid black';
     }
 
+    this.styleoff = function(){
+      this.video.style.border = '0px';
+    }
+
+    this.muted = function(boolean){
+      this.video.muted = boolean;
+    }
   }
 
   var video1 = new video("video1",200,100);
@@ -27,13 +37,25 @@ function main()
   video1.p_play("play1");
   video1.play.onclick = () => {
     video1.principal(video4);
+    video1.styleput();
+    video2.styleoff();
+    video3.styleoff();
   }
+  video1.onmouseover = () => {
+      video1.muted(false);
+  }
+  video1.onmouseout = () => {
+      video1.muted(true);
+    }
 
   var video2 = new video("video2",200,100);
   video2.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4");
   video2.p_play("play2");
   video2.play.onclick = () => {
     video2.principal(video4);
+    video2.styleput();
+    video1.styleoff();
+    video3.styleoff();
   }
 
   var video3 = new video("video3",200,100);
@@ -41,6 +63,9 @@ function main()
   video3.p_play("play3");
   video3.play.onclick = () => {
     video3.principal(video4);
+    video3.styleput();
+    video1.styleoff();
+    video2.styleoff();
   }
 
   function hora(segundos){
@@ -83,5 +108,6 @@ function main()
 /*
 - Si acaba el video y le das a loop ya no funciona
 - no funciona el mute
+-style no funciona meter los dos videos y quitarles el estilo
 */
 }
