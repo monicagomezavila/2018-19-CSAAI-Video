26ff0e08ec03de74ebe8b4bb48cf93eccfb112a0
monicagomezavila <monicagomezavila@github.com>
2019-04-11 18:31:27 +0200

    depurando codio

--- video.js
+++ video.js
@@ -14,77 +14,67 @@ function main()
       this.video.src = url;
     }
 
-    this.principal = function(video4){
+    this.principal = function(video4, v1, v2){
       video4.src  = this.video.src;
       video4.currentTime = this.video.currentTime;
-    }
-
-    this.styleput = function(){
       this.video.style.border = '3px solid black';
-    }
-
-    this.styleoff = function(){
-      this.video.style.border = '0px';
+      v1.style.border = '0px';
+      v2.style.border = '0px';
     }
 
   }
 
   var video1 = new video("video1",200,100);
-
   video1.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4");
   video1.p_play("play1");
   video1.play.onclick = () => {
-    video1.principal(video4);
-    video1.styleput();
-    video2.styleoff();
-    video3.styleoff();
+    video1.principal(video4, video2.video,video3.video);
   }
 
-  video1.video.onmouseover = () => {
-    video1.video.muted = false;
-    console.log(video1.video.paused)
-  }
+//  video1.video.onmouseover = () => {
+//    video1.video.muted = false;
+//    video1.video.play()
+//  }
 
-  video1.video.onmouseout = () => {
-      video1.video.muted = true;
-  }
+//  video1.video.onmouseout = () => {
+//      video1.video.muted = true;
+//      video1.video.play()
+//  }
 
 
   var video2 = new video("video2",200,100);
   video2.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4");
   video2.p_play("play2");
   video2.play.onclick = () => {
-    video2.principal(video4);
-    video2.styleput();
-    video1.styleoff();
-    video3.styleoff();
+    video2.principal(video4, video1.video,video3.video);
   }
 
-  video2.video.onmouseover = () => {
-      video2.video.muted = false;
-  }
+//  video2.video.onmouseover = () => {
+//      video2.video.muted = false;
+//      video2.video.play()
+//  }
 
-  video2.video.onmouseout = () => {
-      video2.video.muted = true;
-  }
+//  video2.video.onmouseout = () => {
+//      video2.video.muted = true;
+//      video2.video.play()
+//  }
 
   var video3 = new video("video3",200,100);
   video3.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4");
   video3.p_play("play3");
   video3.play.onclick = () => {
-    video3.principal(video4);
-    video3.styleput();
-    video1.styleoff();
-    video2.styleoff();
+    video3.principal(video4, video1.video,video2.video);
   }
 
-  video3.video.onmouseover = () => {
-      video3.video.muted = false;
-  }
+//  video3.video.onmouseover = () => {
+//      video3.video.muted = false;
+//      video3.video.play()
+//  }
 
-  video3.video.onmouseout = () => {
-      video3.video.muted = true;
-  }
+//  video3.video.onmouseout = () => {
+//      video3.video.muted = true;
+//      video3.video.play()
+//  }
 
   function hora(segundos){
      var d=new Date(segundos*1000);
@@ -110,22 +100,15 @@ function main()
 
 //VÍDEO INICIAL EN LOOPING O NO
   var loop = document.getElementById('loop');
-
   loop.onclick = () => {
     console.log("looping!");
     video4.loop = true;
   }
 
   var noloop = document.getElementById("noloop");
-
   noloop.onclick = () => {
     console.log("no looping!")
     video4.loop = false;
   }
 
-/*
-- Si acaba el video y le das a loop ya no funciona
-- no funciona el mute
--style no funciona meter los dos videos y quitarles el estilo
-*/
 }
