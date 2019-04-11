24bbb82a395e4381f14eccc5e19e9294e8fb19f9
Monica Gomez Avila <mgavila@beta11.aulas.gsyc.urjc.es>
2019-04-11 10:28:14 +0200

    muted intento de funcionar

--- video.js
+++ video.js
@@ -27,12 +27,18 @@ function main()
       this.video.style.border = '0px';
     }
 
-    this.muted = function(boolean){
-      this.video.muted = boolean;
-    }
   }
 
   var video1 = new video("video1",200,100);
+
+  video1.video.onmouseover = () => {
+      video1.video.muted = false;
+  }
+
+  video1.video.onmouseout = () => {
+      video1.video.muted = true;
+  }
+
   video1.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4");
   video1.p_play("play1");
   video1.play.onclick = () => {
@@ -41,12 +47,7 @@ function main()
     video2.styleoff();
     video3.styleoff();
   }
-  video1.onmouseover = () => {
-      video1.muted(false);
-  }
-  video1.onmouseout = () => {
-      video1.muted(true);
-    }
+
 
   var video2 = new video("video2",200,100);
   video2.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4");
@@ -56,6 +57,14 @@ function main()
     video2.styleput();
     video1.styleoff();
     video3.styleoff();
+  }
+
+  video2.video.onmouseover = () => {
+      video2.video.muted = false;
+  }
+
+  video2.video.onmouseout = () => {
+      video2.video.muted = true;
   }
 
   var video3 = new video("video3",200,100);
