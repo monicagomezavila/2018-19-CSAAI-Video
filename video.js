acf7c7c63841618e713b7e85084cd67d09c386f2
monicagomezavila <monicagomezavila@github.com>
2019-04-13 13:40:17 +0200

    Vídeo principal también con constructor

--- video.js
+++ video.js
@@ -1,6 +1,16 @@
-function main()
-{
+function main(){
+  
+  //--Función para mostrar el tiempo del vídeo
+  function hora(segundos){
+     var d=new Date(segundos*1000);
+     var hora = (d.getHours()==0)?23:d.getHours()-1;
+     var hora = (hora<9)?"0"+hora:hora;
+     var minuto = (d.getMinutes()<9)?"0"+d.getMinutes():d.getMinutes();
+     var segundo = (d.getSeconds()<9)?"0"+d.getSeconds():d.getSeconds();
+     return hora+":"+minuto+":"+segundo;
+    }
 
+  //--Constructor de vídeos
   function video(video, width, height){
     this.video = document.getElementById(video);
     this.video.width = width;
@@ -24,90 +34,89 @@ function main()
 
   }
 
-  var video1 = new video("video1",200,100);
-  video1.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4");
+
+  //--Vídeo 1
+  var video1 = new video("video1",200,150);
+  video1.url("video2.mp4");
   video1.p_play("play1");
   video1.play.onclick = () => {
-    video1.principal(video4, video2.video,video3.video);
+    video1.principal(video4.video, video2.video,video3.video);
   }
 
   video1.video.onmouseover = () => {
     video1.video.muted = false;
     video1.video.play()
   }
-
   video1.video.onmouseout = () => {
       video1.video.muted = true;
       video1.video.play()
   }
 
 
-  var video2 = new video("video2",200,100);
-  video2.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4");
+  //Vídeo 2
+  var video2 = new video("video2",200,150);
+  video2.url("video1.mp4");
   video2.p_play("play2");
   video2.play.onclick = () => {
-    video2.principal(video4, video1.video,video3.video);
+    video2.principal(video4.video, video1.video,video3.video);
   }
 
   video2.video.onmouseover = () => {
       video2.video.muted = false;
       video2.video.play()
   }
-
   video2.video.onmouseout = () => {
       video2.video.muted = true;
       video2.video.play()
   }
 
-  var video3 = new video("video3",200,100);
-  video3.url("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4");
+  //Vídeo 3
+  var video3 = new video("video3",200,150);
+  video3.url("video3.mp4");
   video3.p_play("play3");
   video3.play.onclick = () => {
-    video3.principal(video4, video1.video,video2.video);
+    video3.principal(video4.video, video1.video,video2.video);
   }
 
   video3.video.onmouseover = () => {
       video3.video.muted = false;
       video3.video.play()
   }
-
   video3.video.onmouseout = () => {
       video3.video.muted = true;
       video3.video.play()
   }
 
-  function hora(segundos){
-     var d=new Date(segundos*1000);
-     var hora = (d.getHours()==0)?23:d.getHours()-1;
-     var hora = (hora<9)?"0"+hora:hora;
-     var minuto = (d.getMinutes()<9)?"0"+d.getMinutes():d.getMinutes();
-     var segundo = (d.getSeconds()<9)?"0"+d.getSeconds():d.getSeconds();
-     return hora+":"+minuto+":"+segundo;
-    }
 
-// VÍDEO PRINCIPAL
-  video4 = document.getElementById("video4")
-  video4.width = 600;
-  video4.height= 400;
-  video1.principal(video4, video2.video,video3.video);
+  //--Vídeo principal
+  video4 = new video("video4",600,400);
+  video1.principal(video4.video, video2.video,video3.video);
 
-
-//DURACIÓN DEL VÍDEO
- video4.addEventListener("timeupdate",function(ev){
-   document.getElementById("tduracion").innerHTML = hora(video4.currentTime);
+  //--Duración del vídeo
+ video4.video.addEventListener("timeupdate",function(ev){
+   document.getElementById("tduracion").innerHTML = hora(video4.video.currentTime);
   },true);
+
+  video4.video.onmouseover = () => {
+    video4.video.muted = false;
+    video4.video.play()
+  }
+  video4.video.onmouseout = () => {
+      video4.video.muted = true;
+      video4.video.play()
+  }
 
 
   var loop = document.getElementById('loop');
   loop.onclick = () => {
     console.log("looping!");
-    video4.loop = true;
+    video4.video.loop = true;
   }
 
   var noloop = document.getElementById("noloop");
   noloop.onclick = () => {
     console.log("no looping!")
-    video4.loop = false;
+    video4.video.loop = false;
   }
 
 }
